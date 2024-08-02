622cedfea249e4f41681ed6d6d72c59f17173272
suraj singh rajput <surajthakurrs45@gmail.com>
2024-08-02 10:37:24 +0530

    header created

--- src/components/Header/page.js
+++ src/components/Header/page.js
@@ -1,39 +1,52 @@
 import Image from "next/legacy/image";
 import Images from "../../constants/Images.js";
 import Link from "next/link";
-import { IoSearchOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
-import { BsCart2 } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import { IoMdSearch } from "react-icons/io";
 function page() {
   return (
-    <header className="flex items-center p-4 border-b">
+    <header className="flex items-center p-4 border-b sm:flex-wrap">
       <div class="flex items-center gap-2">
-        <div class="logo relative w-[70px] h-[50px] ">
+        <div class="logo relative w-[70px] h-[50px] sm:h-[20px] sm:w-[20px]">
           <Link href={"/"}>
             <Image src={Images.icon.src} alt="logo" layout="fill" />
           </Link>
         </div>
-        <div class="text-xl">
+        <div class="text-2xl font-semibold sm:text-sm">
           <Link href={"/"}>
             BYTE <span className="text-skyblue-900">BAZAAR</span>
           </Link>
         </div>
       </div>
-      <div class="flex items-center gap-2 ">
-        <li>Home</li>
-        <li>Shop</li>
-        <li>Category</li>
-        <li>Product</li>
+
+      <div class="flex items-center gap-10 flex-1 justify-center text-2xl font-semibold text-[#3b3b3b] sm:py-2 sm:pl-2 sm:text-sm sm:gap-4 ">
+        <li>
+          <Link href="">Home</Link>
+        </li>
+        <li>
+          <Link href="">Shop</Link>
+        </li>
+        <li>
+          <Link href="">Category</Link>
+        </li>
+        <li>
+          <Link href="">Product</Link>
+        </li>
       </div>
-      <div class="flex items-center gap-2 ">
+      <div class="flex items-center gap-5 w-[20%] text-2xl justify-end sm:justify-center sm:w-[100%]  sm:py-2 sm:gap-2 sm:text-sm pr-4">
         <div class="icons">
-          <IoSearchOutline />
+          <IoMdSearch />
         </div>
         <div class="icons">
           <FaRegUser />
         </div>
-        <div class="icons">
-          <BsCart2 />
+        <div class="icons relative">
+          <div class=" rounded-full absolute -top-3 right-0 text-sm text-black font-semibold">0</div>
+          <i className="cursor-pointer">
+            <FiShoppingCart />
+          </i>
         </div>
       </div>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,28 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      "4xl": { max: "2047px" },
+      // => @media (max-width: 2047px) { ... }
+
+      "3xl": { max: "1791px" },
+      // => @media (max-width: 1791px) { ... }
+
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
     extend: {
       colors: {
           skyblue: {
